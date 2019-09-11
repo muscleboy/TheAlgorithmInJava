8edabe3f0d38940cc68929eceed41dd80c1e4860
muscleboy <1076146831@qq.com>
2019-09-11 18:42:07 +0800

    快速排序

--- src/main/java/xyz/bugcoder/sort/QuickSort.java
+++ src/main/java/xyz/bugcoder/sort/QuickSort.java
@@ -1,8 +1,6 @@
 package xyz.bugcoder.sort;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Create with IDEA.
@@ -16,15 +14,45 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
-        int[] arr = new int[]{1,534,2,54,67,434,576,45};
+        int[] arr = new int[]{6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
         System.out.println(Arrays.toString(arr));
-        quickSort(arr);
+        quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void quickSort(int[] arr){
+    public static void quickSort(int[] arr, int left, int right){
 
+        // 递归退出条件
+        if (left < right){
 
+            // 左右指针
+            int low = left;
+            int high = right;
+            // 把第一位作为 基准值
+            int stand = arr[left];
+
+            // 循环比较
+            while (low != high){
+
+                // 从右 --> 左，如果 >= stand，向左移一位
+                while (arr[high] >= stand && low < high)
+                    high --;
+                // 否则将arr[high] 替换 arr[low]
+                arr[low] = arr[high];
+                // 从左 --> 右，如果 <= stand，向右移一位
+                while (arr[low] <= stand && low < high)
+                    low ++;
+                // 否则将arr[low] 替换 arr[high]
+                arr[high] = arr[low];
+            }
+
+            // 此时low = high，将stand赋给low位置
+            arr[low] = stand;
+            // 递归排序左边
+            quickSort(arr, left, low);
+            // 递归排序右边
+            quickSort(arr, low + 1, right);
+        }
     }
 
 }
