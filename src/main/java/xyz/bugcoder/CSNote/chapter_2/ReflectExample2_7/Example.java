864b7d2a0d11090c76c1bce218596371b41dd636
muscleboy <1076146831@qq.com>
2019-10-14 11:47:26 +0800

    Java反射

---
+++ src/main/java/xyz/bugcoder/CSNote/chapter_2/ReflectExample2_7/Example.java
@@ -0,0 +1,73 @@
+package xyz.bugcoder.CSNote.chapter_2.ReflectExample2_7;
+
+/**
+ * Create with IDEA.
+ *
+ * @Package: xyz.bugcoder.CSNote.chapter_2.ReflectExample2_7
+ * @Description:
+ * @Date: 2019-10-14 10:22
+ * @Author: Wyj
+ */
+public class Example {
+
+    public int id;
+    public String name;
+
+    @Override
+    public String toString() {
+        return "Example{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int add(int a, int b){
+
+        return a + b;
+    }
+
+    private void privateMethod(String a, int b){
+
+        System.out.println(a + " " + b);
+    }
+
+    protected void protectedMethod(String a, int b){
+
+        System.out.println(a + " " + b);
+    }
+
+    void defaultMethod(String a, int b){
+
+        System.out.println(a + " " + b);
+    }
+
+    public Example(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Example() {
+        System.out.println("构造函数");
+    }
+
+    public void show(){
+
+        System.out.println("show方法....");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/xyz/bugcoder/CSNote/chapter_2/ReflectExample2_7/ReflectExample.java
+++ src/main/java/xyz/bugcoder/CSNote/chapter_2/ReflectExample2_7/ReflectExample.java
@@ -1,18 +1,61 @@
 package xyz.bugcoder.CSNote.chapter_2.ReflectExample2_7;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Create with IDEA.
  *
  * @Package: xyz.bugcoder.CSNote.chapter_2.ReflectExample2_7
- * @Description:
+ * @Description: Java反射
  * @Date: 2019-10-12 17:09
  * @Author: Wyj
  */
 public class ReflectExample {
 
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
-        Class<?> c = String.class;
-        Object o = c.newInstance();
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+
+        // 获取到Class对象
+        Class<Example> exampleClass = Example.class;
+
+        // 可以获取到这个类的所有public方法，包括继承过来的
+//        Method[] methods = exampleClass.getMethods();
+//        for (Method m : methods) {
+//            System.out.println(m);
+//        }
+
+        // 可以获取到自己定义的方法，不管什么权限
+        Method[] ms = exampleClass.getDeclaredMethods();
+        for (Method m : ms) {
+            System.out.println(m);
+        }
+
+        Constructor<Example> constructor = exampleClass.getConstructor();
+        Example o = constructor.newInstance();
+//        o.setId(10);
+//        o.setName("1234");
+        System.out.println(o);
+
+        // 通过反射执行方法
+//        Method m = exampleClass.getMethod("add", int.class, int.class);
+//        System.out.println("通过反射执行add方法，结果是： " + m.invoke(o, 1, 3));
+//
+//        // 通过反射执行private方法，getDeclaredMethod()才能获取到
+//        Method m2 = exampleClass.getDeclaredMethod("privateMethod", String.class, int.class);
+//        // 只是获取访问权，并不是修改实际权限
+//        m2.setAccessible(true);
+//        m2.invoke(o, "Java reflect", 6666);
+
+        // 通过newInstance()创建一个对象，会执行空的构造函数
+//        Example example = exampleClass.newInstance();
+
+        // 通过反射获取成员变量，只能获取到Public修饰的
+        Field[] fields = exampleClass.getFields();
+        for (Field f : fields) {
+            System.out.println(f);
+        }
     }
 
 }
