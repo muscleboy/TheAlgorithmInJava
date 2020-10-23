af0ccd5df438024436a9b58f31f1a95174f9a6b4
muscleboy <1076146831@qq.com>
2020-10-23 16:08:40 +0800

    【add】迪米特法则

---
+++ src/main/java/xyz/bugcoder/jianzhioffer/LinkedList/DeleteRepeatNode_18.java
@@ -0,0 +1,11 @@
+package xyz.bugcoder.jianzhioffer.LinkedList;
+
+/**
+ * @Package: xyz.bugcoder.jianzhioffer.LinkedList
+ * @author: Weiyj
+ * @Description: 删除链表中重复的节点
+ * @createTime 2020-10-23 08:59
+ */
+public class DeleteRepeatNode_18 {
+
+}
