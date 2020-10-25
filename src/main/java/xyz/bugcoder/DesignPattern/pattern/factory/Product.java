a6b3e58fa90153f4fc98d85e2ad660dd4b11db50
muscleboy <1076146831@qq.com>
2020-10-25 18:08:58 +0800

    【add】

---
+++ src/main/java/xyz/bugcoder/DesignPattern/pattern/factory/Product.java
@@ -0,0 +1,13 @@
+package xyz.bugcoder.DesignPattern.pattern.factory;
+
+/**
+ * @Package: xyz.bugcoder.DesignPattern.pattern.factory
+ * @author: Weiyj
+ * @Description: TODO
+ * @createTime 2020-10-25 17:11
+ */
+public interface Product {
+
+    void show();
+
+}
---
+++ src/main/java/xyz/bugcoder/DesignPattern/pattern/factory/ProductA.java
@@ -0,0 +1,15 @@
+package xyz.bugcoder.DesignPattern.pattern.factory;
+
+/**
+ * @Package: xyz.bugcoder.DesignPattern.pattern.factory
+ * @author: Weiyj
+ * @Description: TODO
+ * @createTime 2020-10-25 17:12
+ */
+public class ProductA implements Product {
+
+    public void show() {
+        System.out.println("Product A");
+    }
+
+}
