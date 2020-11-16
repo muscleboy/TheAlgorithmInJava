eee6e68de955c83f32de0d1daaa4f5a5837f7f48
muscleboy <1076146831@qq.com>
2020-11-16 12:42:51 +0800

    【add】leetcode 填充树的下一节点 mid 116

---
+++ src/main/java/xyz/bugcoder/leetcode_pp/tree/FillNextTreeNode_Mid_116.java
@@ -0,0 +1,131 @@
+package xyz.bugcoder.leetcode_pp.tree;
+
+//给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+//
+// struct Node {
+//  int val;
+//  Node *left;
+//  Node *right;
+//  Node *next;
+//}
+//
+// 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+//
+// 初始状态下，所有 next 指针都被设置为 NULL。
+//
+//
+//
+// 示例：
+//
+//
+//
+// 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"ri
+//ght":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right
+//":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left"
+//:null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":nu
+//ll,"next":null,"right":null,"val":7},"val":3},"val":1}
+//
+//输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4
+//","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next"
+//:null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":
+//null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":
+//"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"va
+//l":1}
+//
+//解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 你只能使用常量级额外空间。
+// 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+//
+// Related Topics 树 深度优先搜索 广度优先搜索
+// 👍 338 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+import xyz.bugcoder.leetcode_pp.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Package: xyz.bugcoder.leetcode_pp.tree
+ * @author: Weiyj
+ * @Description: TODO
+ * @createTime 2020-11-16 11:58
+ */
+public class FillNextTreeNode_Mid_116 {
+
+    public static Node connect(Node root){
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                Node node = queue.poll();
+                // < size - 1，控制该层最后一个指向null(没指向那就是null)
+                if (i < size - 1){
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+        Node n7 = new Node(7);
+
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+
+        System.out.println(connect(n1));
+    }
+
+}
