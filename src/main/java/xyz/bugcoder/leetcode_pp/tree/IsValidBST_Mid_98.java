124921c73ab22cb78947e347fa3459d7e46a7bf6
muscleboy <1076146831@qq.com>
2020-11-26 17:46:38 +0800

    【add】leetcode 98 是否合法的二叉搜索树

--- src/main/java/xyz/bugcoder/leetcode_pp/tree/IsValidBST_Mid_98.java
+++ src/main/java/xyz/bugcoder/leetcode_pp/tree/IsValidBST_Mid_98.java
@@ -48,11 +48,23 @@ import xyz.bugcoder.leetcode_pp.TreeNode;
 public class IsValidBST_Mid_98 {
 
     // 判断是否为二叉搜索树
+    // 思路
+    //   dfs，不过要注意右子树左节点的取值范围
+    //   当前节点的值为 左子树的上限
+    //   当前节点的值为 右子树的下限
+    // 复杂度
+    //   时间：O(logN)
+    //   空间：O(logN)，递归栈空间
     public boolean isValidBST(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+
         return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
     public boolean isValidBST(TreeNode root, Integer minVal, Integer maxVal) {
+        //
         if (root == null){
             return true;
         }
@@ -64,11 +76,23 @@ public class IsValidBST_Mid_98 {
         //    6   9
 
         // 如这里的6，一定要 5 < 6 < 8
+        // 所以这里 直接反向判断
         if (root.val <= minVal || root.val >= maxVal){
             return false;
         }
 
         return isValidBST(root, minVal, root.val) && isValidBST(root, root.val, maxVal);
+    }
+
+    // 这里有个坑，就是右子树左节点的范围问题，以下为 错误代码！！！！
+    public boolean isValidBST_Wrong(TreeNode root){
+        if (root == null){
+            return true;
+        }
+        if ((root.left == null && root.right.val > root.val) || (root.right == null && root.left.val < root.val)){
+            return true;
+        }
+        return isValidBST(root.left) && isValidBST(root.right);
     }
 
 }
