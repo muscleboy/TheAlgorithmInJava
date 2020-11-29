d3d81560cd542018b78e9879b219af2e15524311
muscleboy <1076146831@qq.com>
2020-11-29 16:14:11 +0800

    【add】带聚焦策略线程池

--- src/main/java/xyz/bugcoder/concurrent/heima_8_tools/BlockingQueue.java
+++ src/main/java/xyz/bugcoder/concurrent/heima_8_tools/BlockingQueue.java
@@ -15,6 +15,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * @Description: TODO
  * @createTime 2020-11-28 17:10
  */
+@Slf4j
 class BlockingQueue<T> {
 
     // 1.任务队列
@@ -56,6 +57,31 @@ class BlockingQueue<T> {
         }
     }
 
+    // 带超时阻塞获取
+    public T poll(long timeout, TimeUnit unit){
+        lock.lock();
+        try {
+            long nanos = unit.toNanos(timeout);
+            // 空的队列，就阻塞
+            while (queue.isEmpty()){
+                try {
+                    if (nanos <= 0){
+                        return null;
+                    }
+                    nanos = consumer.awaitNanos(nanos);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            T t = queue.removeFirst();
+            producer.signal();
+            // 不是空的，从头拿
+            return t;
+        }  finally {
+            lock.unlock();
+        }
+    }
+
     // 7.生产
     public void put(T e){
         lock.lock();
@@ -63,13 +89,41 @@ class BlockingQueue<T> {
             // 满了，
             while (queue.size() == capacity){
                 try {
+                    log.info("等待加入任务队列 {}...", e);
                     producer.await();
                 } catch (InterruptedException e1) {
                     e1.printStackTrace();
                 }
             }
+            log.info("加入任务队列 {}...", e);
             queue.addLast(e);
             consumer.signal();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    // 7.生产带超时时间
+    public boolean offer(T task, long timeout, TimeUnit unit){
+        lock.lock();
+        try {
+            long nanos = unit.toNanos(timeout);
+            // 满了，
+            while (queue.size() == capacity){
+                try {
+                    if (nanos <= 0){
+                        return false;
+                    }
+                    log.info("等待加入任务队列 {}...", task);
+                    nanos = producer.awaitNanos(nanos);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            log.info("加入任务队列 {}...", task);
+            queue.addLast(task);
+            consumer.signal();
+            return true;
         }finally {
             lock.unlock();
         }
@@ -84,6 +138,28 @@ class BlockingQueue<T> {
             lock.unlock();
         }
     }
+
+    public void tryPut(RejectPolicy<T> rejectPolicy, T task) {
+        lock.lock();
+        try {
+            if (queue.size() == capacity){
+                rejectPolicy.reject(this, task);
+            }else {
+                log.info("加入任务队列 {}...", task);
+                queue.addLast(task);
+                consumer.signal();
+            }
+        }finally {
+            lock.unlock();
+        }
+    }
+}
+
+// 拒绝策略
+@FunctionalInterface
+interface RejectPolicy<T>{
+
+    void reject(BlockingQueue<T> queue, T task);
 
 }
 
@@ -105,11 +181,14 @@ class ThreadPool{
 
     private TimeUnit unit;
 
-    public ThreadPool(int coreSize, long timeout, TimeUnit unit, int queueSize) {
+    private RejectPolicy<Runnable> rejectPolicy;
+
+    public ThreadPool(int coreSize, long timeout, TimeUnit unit, int queueSize, RejectPolicy<Runnable> rejectPolicy) {
         this.coreSize = coreSize;
         this.timeout = timeout;
         this.unit = unit;
         this.taskQueue = new BlockingQueue<>(queueSize);
+        this.rejectPolicy = rejectPolicy;
     }
 
     public void execute(Runnable task){
@@ -121,7 +200,14 @@ class ThreadPool{
                 workers.add(worker);
                 worker.start();
             }else {
-                taskQueue.put(task);
+                // taskQueue.put(task);
+                // 队列满了，该执行哪种策略？
+                // 1.死等
+                // 2.带超时等待
+                // 3.让调用者放弃任务执行
+                // 4.让调用者抛异常
+                // 5.让调用者自己执行任务
+                taskQueue.tryPut(rejectPolicy, task);
             }
         }
     }
@@ -139,7 +225,10 @@ class ThreadPool{
             // 执行任务
             // 1.执行当前的 task
             // 2.执行任务队列中的 task
-            while (task != null || (task = taskQueue.take()) != null){
+//            while (task != null || (task = taskQueue.take()) != null){
+
+            // 超时时间
+            while (task != null || (task = taskQueue.poll(timeout, unit)) != null){
                 try {
                     log.info("任务运行.... {}", task);
                     task.run();
@@ -149,7 +238,7 @@ class ThreadPool{
             }
 
             synchronized (workers){
-                log.info("完成 work: {}", workers);
+                log.debug("worker 被移除{}", this);
                 workers.remove(this);
             }
 
@@ -157,9 +246,20 @@ class ThreadPool{
     }
 
     public static void main(String[] args) {
-        ThreadPool pool = new ThreadPool(2, 1, TimeUnit.SECONDS, 5);
+        ThreadPool pool = new ThreadPool(1, 1, TimeUnit.SECONDS, 1, ((queue, task) -> {
+            // 1.死等
+//            queue.put(task);
+            // 2.带超时等待
+//            queue.offer(task, 1500, TimeUnit.MILLISECONDS);
+            // 3.让调用者放弃任务执行
+//            log.info("放弃 {}...", task);
+            // 4.让调用者抛异常
+//            throw new RuntimeException("抛异常跳过..." + task);
+            // 5.让调用者(main线程)自己执行任务
+            task.run();
+        }));
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 3; i++) {
             int j = i;
             pool.execute(() -> {
                 try {
