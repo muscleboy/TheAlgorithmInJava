83a4a94ff8bbb18ea157fc0f4ca66aa8a1f50c61
muscleboy <1076146831@qq.com>
2020-12-26 13:05:31 +0800

    dfs-岛屿的数量 m 200

---
+++ src/main/java/xyz/bugcoder/leetcode_pp/bfs/NumIslands_Mid_200.java
@@ -0,0 +1,88 @@
+package xyz.bugcoder.leetcode_pp.bfs;
+
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+//
+// 此外，你可以假设该网格的四条边均被水包围。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+//
+//
+// 示例 2：
+//
+//
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 300
+// grid[i][j] 的值为 '0' 或 '1'
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集
+// 👍 915 👎 0
+
+/**
+ * @Package: xyz.bugcoder.leetcode_pp.bfs
+ * @author: Weiyj
+ * @Description: TODO
+ * @createTime 2020-12-26 12:59
+ */
+public class NumIslands_Mid_200 {
+
+    // 思路
+    //   找到[i][j] = 1的位置，然后 dfs 将其上下左右置为 0
+    // 复杂度
+    //   时间：O(N²)
+    //   空间：O(N²) ?
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    count ++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void dfs(char[][] grid, int i, int j){
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
+            return;
+        }
+        // 如果[i][j]是1，那它的上下左右跟它相连
+        // 只能算一个，所有要把它的上下左右置为0
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+}
