8e6f5d854a6ce9a0460850f6bcf0c48b1bd69c3a
muscleboy <1076146831@qq.com>
2020-12-29 23:01:40 +0800

    bfs回溯-单词搜索 m 79

---
+++ src/main/java/xyz/bugcoder/leetcode_pp/back_track/Exist_Mid_79.java
@@ -0,0 +1,87 @@
+package xyz.bugcoder.leetcode_pp.back_track;
+
+//给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+//
+// 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+//
+//
+//
+// 示例:
+//
+// board =
+//[
+//  ['A','B','C','E'],
+//  ['S','F','C','S'],
+//  ['A','D','E','E']
+//]
+//
+//给定 word = "ABCCED", 返回 true
+//给定 word = "SEE", 返回 true
+//给定 word = "ABCB", 返回 false
+//
+//
+//
+// 提示：
+//
+//
+// board 和 word 中只包含大写和小写英文字母。
+// 1 <= board.length <= 200
+// 1 <= board[i].length <= 200
+// 1 <= word.length <= 10^3
+//
+// Related Topics 数组 回溯算法
+// 👍 716 👎 0
+
+import org.junit.Test;
+
+/**
+ * @Package: xyz.bugcoder.leetcode_pp.back_track
+ * @author: Weiyj
+ * @Description: TODO
+ * @createTime 2020-12-29 22:25
+ */
+public class Exist_Mid_79 {
+
+    // 思路
+    //   思路和岛屿的数量(200)类似，走到一个位置，就 dfs 向上下左右走
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, word, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean dfs(char[][] board, String word, int i, int j, int index){
+        // 越界处理以及判断 [i][j] 位置是否为 word[index]
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)){
+            return false;
+        }
+        // 终止条件
+        if (index == word.length() - 1) {
+            return true;
+        }
+        char temp = board[i][j];
+        // 选择，把 [i][j]置为 '0'
+        board[i][j]   = '0';
+        boolean res = dfs(board, word, i + 1, j, index + 1)
+                || dfs(board, word, i - 1, j, index + 1)
+                || dfs(board, word, i, j + 1, index + 1)
+                || dfs(board, word, i, j - 1, index + 1);
+        // 撤销(回溯)
+        board[i][j] = temp;
+        return res;
+    }
+
+    @Test
+    public void test(){
+        char[][] board = {{'C', 'A', 'A'}, {'A', 'A', 'A'}, {'B', 'C', 'D'}};
+        String s = "AAB";
+        System.out.println(exist(board, s));
+    }
+
+}
