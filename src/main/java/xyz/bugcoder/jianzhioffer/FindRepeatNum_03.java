389e6194077a8db9ea718f725f5aacfc22997c50
muscleboy <1076146831@qq.com>
2021-02-04 18:23:42 +0800

    剑指 Offer 03. 数组中重复的数字 e

--- src/main/java/xyz/bugcoder/jianzhioffer/FindRepeatNum_03.java
+++ src/main/java/xyz/bugcoder/jianzhioffer/FindRepeatNum_03.java
@@ -1,52 +1,76 @@
 package xyz.bugcoder.jianzhioffer;
 
-import java.util.Arrays;
+import org.junit.Test;
+
+//找出数组中重复的数字。
+//
+//
+//在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请
+//找出数组中任意一个重复的数字。
+//
+// 示例 1：
+//
+// 输入：
+//[2, 3, 1, 0, 2, 5, 3]
+//输出：2 或 3
+//
+//
+//
+//
+// 限制：
+//
+// 2 <= n <= 100000
+// Related Topics 数组 哈希表
+// 👍 265 👎 0
 
 /**
  * Create with IDEA.
  *
  * @Package: xyz.bugcoder.jianzhioffer
- * @Description:
-     * 面试题3（一）：找出数组中重复的数字
-     * 题目：在一个长度为n的数组里的所有数字都在0到n-1的范围内。数组中某些数字是重复的，
-     * 但不知道有几个数字重复了，
-     * 也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
-     * 例如，如果输入长度为7的数组{2, 3, 1, 0, 2, 5, 3}，
-     * 那么对应的输出是重复的数字2或者3。
  * @Date: 2019-09-28 16:46
  * @Author: Wyj
  */
 public class FindRepeatNum_03 {
 
-    public static boolean findRepeatNum(int[] arr){
+    /**
+     * 思路
+     *    根据题意，所有数字都在 0～n-1 的范围内，
+     *    遍历数组，扫描到下标为 i 的数字时，看看找个数是不是 i
+     *    不是就交换
+     * 复杂度
+     *    时间：O(N)
+     *    空间：O(1)
+     * @param arr
+     * @return
+     */
+    public int findRepeatNum(int[] arr) {
 
-        // 输出原数组的元素
-        System.out.println(Arrays.toString(arr));
         // 遍历数组的每一位元素
         for (int i = 0; i < arr.length - 1; i++) {
             // 当数组arr[i](第i位置)的值 不等于 下标i的值
-            while (arr[i] != i){
+            while (arr[i] != i) {
                 // 第i位置的值 不等于 第arr[i]位置的值时，交换位置
-                if (arr[i] != arr[arr[i]]){
+                if (arr[i] != arr[arr[i]]) {
 
                     int temp = arr[i];
                     arr[i] = arr[temp];
                     arr[temp] = temp;
-                }else {
+                }
+                else {
                     // 相等时，说明遇到了重复的元素，输出重复的元素
-                    System.out.println(arr[i]);
-                    return true;
+                    return arr[i];
                 }
             }
         }
 
-        return false;
+        return -1;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void test() {
 
         int[] arr = {2, 3, 1, 0, 6, 5, 4, 4, 5};
-        findRepeatNum(arr);
+        System.out.println(findRepeatNum(arr));
     }
 
 }
