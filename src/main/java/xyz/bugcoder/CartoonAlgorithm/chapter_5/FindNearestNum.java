ab3ab21bfea88bbbc9b08fa63b0b094765a5cb67
muscleboy <1076146831@qq.com>
2021-02-06 11:45:35 +0800

    31. 下一个排列 m

--- src/main/java/xyz/bugcoder/CartoonAlgorithm/chapter_5/FindNearestNum.java
+++ src/main/java/xyz/bugcoder/CartoonAlgorithm/chapter_5/FindNearestNum.java
@@ -1,18 +1,65 @@
 package xyz.bugcoder.CartoonAlgorithm.chapter_5;
 
+import org.junit.Test;
+
 import java.util.Arrays;
+
+//实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+//
+// 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+//
+// 必须 原地 修改，只允许使用额外常数空间。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [3,2,1]
+//输出：[1,2,3]
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [1,1,5]
+//输出：[1,5,1]
+//
+//
+// 示例 4：
+//
+//
+//输入：nums = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 100
+//
+// Related Topics 数组
+// 👍 929 👎 0
 
 /**
  * @Package: xyz.bugcoder.CartoonAlgorithm.chapter_5
  * @author: Weiyj
- * @Description: 给出一个数，找出这个正整数所有数字全排列的下一个数
- * 在一个证书所包含的数字的组合中，找到一个大于且仅大于原数的新整数
  * @createTime 2020-09-09 07:29
  */
 public class FindNearestNum {
 
     // O(n)
-    public static int[] findNearestNum(int[] nums){
+    public int[] findNearestNum(int[] nums) {
         System.out.println("原数组 ：" + Arrays.toString(nums));
         // 1. 找出逆序区域的前一位index
         int index = 0;
@@ -49,14 +96,61 @@ public class FindNearestNum {
         return nums;
     }
 
+    /**
+     * 思路
+     *    1.找到逆序区域前一个位置 index
+     *    2.交换位置 swap(nums, index-1, min(nums[index:])
+     *    3.reverse(nums, index, n)
+     * 复杂度
+     *    时间：O(N)
+     *    空间：O(1)
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int index = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i > 0 && nums[i] > nums[i - 1]) {
+                index = i;
+                break;
+            }
+        }
 
-    public static void main(String[] args) {
+        // 没找到(已经是有序)
+        if (index == -1) {
+            reverse(nums, 0, n - 1);
+        }
+        else {
+            int k = index;
+            while (k + 1 < n && nums[k + 1] > nums[index - 1]) {
+                k++;
+            }
+            swap(nums, index - 1, k);
+            reverse(nums, index, n - 1);
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
+    }
+
+    @Test
+    public void test() {
         // 全排列总数：5*4*3*2*1 = 120
-        int[] nums = {1,2,3,5,4};
+        int[] nums = {1, 2, 3, 5, 4};
 //        for (int i = 0; i < 10; i++) {
 //            findNearestNum(nums);
 //        }
-        findNearestNum(nums);
+//        findNearestNum(nums);
+        nextPermutation(nums);
     }
 
 }
